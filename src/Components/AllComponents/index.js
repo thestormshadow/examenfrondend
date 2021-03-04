a5d5600f64c25c3e8edbab5560530dbd70ee3e6e
Javier Vargas Ruiz <javiervargasruiz94@gmail.com>
2021-03-04 01:58:51 -0600

    upload

---
+++ src/Components/AllComponents/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+import Car from '../Car'
+
+function  AllComponents(){
+    const [cars, setCars] = useState([]);
+
+    useEffect(() => {
+        getAllCars();    
+    }, []) 
+    
+    const getAllCars  = () => {
+        axios.get('http://localhost:3001/cars')
+        .then(response  => {         
+            setCars(response.data);
+        })   
+    }
+
+    return (
+        cars && (
+        <div>    
+            { cars.map((carrObj, index) => <Car key={index} data={carrObj}></Car>) } 
+        </div>
+        )
+    )
+}
+
+export default AllComponents;
---
+++ src/Components/Car/index.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import { useEffect,useState } from 'react'
+import '../../App.css';
+import axios from 'axios';
+
+function Car(props){
+
+    const [phaseSend, setphase] = useState(0);
+    const [person, setperson] = useState("");
+    const [date, setdate] = useState("");
+
+    const [personMaintenance, setpersonMaintenance] = useState("");
+    const [dateMaintenance, setdateMaintenance] = useState("");
+
+    useEffect(()=>{
+        if(props.data.inMaintenance){
+            axios.get('http://localhost:3001/maintenancePerson/'+props.data.id)
+        .then(response  => {  
+            setphase(2);  
+            console.log(response);
+            setpersonMaintenance(response.data[0].person);
+            setdateMaintenance(response.data[0].date);   
+        })          
+        }
+            
+    },[]) 
+
+    const validate = () =>{
+        if (person == ""){
+            alert("Ingresa una person");
+            return false;
+        }
+        if (date == ""){
+            alert("Ingresa una date");
+            return false;
+        }
+
+        return true;        
+    }
+
+    const save  = () => {
+        if(!validate())
+            return false;
+        
+        console.log("IdAuto: "+props.data.id+" person:"+person+" date:"+date);
+
+        axios.post('http://localhost:3001/register', {
+            idCar: props.data.id,
+            person: person,
+            date: date,
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+
+        clear()  
+        setphase(3);  
+    }
+
+    const clear  = () => {
+        setperson("");
+        setdate("");
+        setphase((phaseSend == 0)?1:0)     
+    }
+
+    return (
+        props.data && (
+        <div className={phaseSend==0?"Card":"CardRed"}> 
+            <div className="Make">  
+                {props.data.make}<br/>
+            </div>
+            <div>  
+                {props.data.model}<br/>
+            </div>
+            <div>
+                <img className="carrImg" src={"http://localhost:3000/img/"+props.data.image} />
+            </div>
+            <div>
+                Description: {props.data.description}
+            </div>
+            <div>
+                KM: {props.data.id}
+            </div>
+            <div>
+                Estimate: {props.data.estimatedate!=""?props.data.estimatedate:"Not Defined"}
+            </div>            
+            <div>
+                ID: {props.data.id}
+            </div>
+            <button className={(phaseSend == 0)?"btn":"btn NotDisplayExtraInfo"} onClick={()=>setphase((phaseSend == 0)?1:0)}>Mark in Maintenance</button>
+            <div className={phaseSend == 1?"":"NotDisplayExtraInfo"}>
+                Person: <input type="text" value={person} onChange={evt => setperson(evt.target.value)}/><br/>
+                
+                Estimated date: <input type="date" value={date} onChange={evt => setdate(evt.target.value)}/><br/>
+                <button onClick={save} className="btnOptions">Guardar</button>
+                <button onClick={clear} className="btnOptions">Cancelar</button>
+                <br/>                
+            </div>
+            <div className={(phaseSend == 2)?"":"NotDisplayExtraInfo"}>
+                In Maintenance by {personMaintenance} and estimated date is {dateMaintenance}
+            </div>
+            <div>--</div>
+        </div>
+        )
+    )
+}
+
+export default Car;
